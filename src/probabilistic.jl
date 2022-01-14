1ee483d51e3a9c622c7c58fee4725f484464d6d8
Ming-Chiang Chang <ming@mingchiangsair.lan>
2022-01-14 16:03:30 -0500

    Resolve hessian not positive semidefinite issue

--- src/probabilistic.jl
+++ src/probabilistic.jl
@@ -5,75 +5,95 @@
 function log_marginal_likelihood(node::Node, θ::AbstractVector, x::AbstractVector,
 								 y::AbstractVector, std_noise::RealOrVec,
 								 mean_θ::RealOrVec, std_θ::RealOrVec)
-	H = hessian_of_objective(node, θ, x, y, std_noise, mean_θ, std_θ)
+	log_θ = log.(θ)
+	H = hessian_of_objective_wrt_log(node, log_θ, x, y, std_noise, mean_θ, std_θ)
 	# calculate marginal likelihood
 	Σ⁻¹ = H # reinterpret Hessian of minimization problem as inverse of covariance matrix
-	negative_log_marginal_likelihood(Σ⁻¹, θ) # TODO: do we need to scale by "height" of distribution?
+	negative_log_marginal_likelihood(Σ⁻¹, log_θ) # TODO: do we need to scale by "height" of distribution?
 end
 
-using ForwardDiff
-using CrystalShift: res!, remove_act_from_θ
 # computes Hessian of objective function - including regularzation - w.r.t. θ
 # useful for Laplace approximation
 # NOTE: if we want to use a separate std_noise for each q value, need to
 # modify the residual function
-using CrystalShift: _residual!, _prior
 function hessian_of_objective(node::Node, θ::AbstractVector, x::AbstractVector,
 							  y::AbstractVector, std_noise::RealOrVec,
 							  mean_θ::RealOrVec, std_θ::RealOrVec)
 	function f(θ)
-		sos_objective(node, θ, x, y, std_noise) + log_regularizer(θ, mean_θ, std_θ)
+		sos_objective(node, θ, x, y, std_noise) + regularizer(θ, mean_θ, std_θ)
 	end
-	println("final sos: $(sos_objective(node, θ, x, y, std_noise))")
-	println("final prior: $(log_regularizer(θ, mean_θ, std_θ))")
-	println("final objective: $(f(θ))")
-	gradient = ForwardDiff.gradient(f, θ)
-	hessian = ForwardDiff.hessian(f, θ)
-	println("gradient: $(gradient)")
-	println("newton step: $(hessian \ gradient)")
-	hessian
+	
+	ForwardDiff.hessian(f, θ)
 end
 
 # sos = sum of squares
 function sos_objective(node::Node, θ::AbstractVector, x::AbstractVector,
-				  	   y::AbstractVector, std_noise::RealOrVec)
+	y::AbstractVector, std_noise::RealOrVec)
 	r = similar(y)
 	r = _residual!(node.current_phases, log.(θ), x, y, r, std_noise)
-	# residual = copy(y)
-	# cps = node.current_phases
-	# res!(cps, θ, x, residual)
-	# residual ./= sqrt(2) * std_noise
-
 	return sum(abs2, r)
 end
 
-function regularizer(node::Node, θ::AbstractVector, mean_θ::RealOrVec, std_θ::RealOrVec)
-    # θ_c = remove_act_from_θ(θ, node.current_phases)
-	# par = @. (θ_c - mean_θ) / (sqrt(2) * std_θ)
-	par = @. (θ - mean_θ) / (sqrt(2) * std_θ)
-	sum(abs2, par)
-end
-
 # regularizer in log space
-function log_regularizer(θ::AbstractVector, mean_θ::RealOrVec, std_θ::RealOrVec)
-    # θ_c = remove_act_from_θ(θ, node.current_phases)
-	# par = @. (log(θ_c) - log(mean_θ)) / (sqrt(2) * std_θ)
-	# par = @. (log(θ) - log(mean_θ)) / (sqrt(2) * std_θ)
-	# sum(abs2, par)
+function regularizer(θ::AbstractVector, mean_θ::RealOrVec, std_θ::RealOrVec)
 	p = similar(θ)
 	sum(abs2, _prior(p, log.(θ), mean_θ, std_θ))
 end
 
+function hessian_of_objective_wrt_log(node::Node, log_θ::AbstractVector, x::AbstractVector,
+									  y::AbstractVector, std_noise::RealOrVec,
+									  mean_θ::RealOrVec, std_θ::RealOrVec)
+	function f(θ)
+		sos_log_objective(node, log_θ, x, y, std_noise) + log_regularizer(θ, mean_θ, std_θ)
+	end
+
+	ForwardDiff.hessian(f, log_θ)
+end
+
+function sos_log_objective(node::Node, log_θ::AbstractVector, x::AbstractVector,
+	                       y::AbstractVector, std_noise::RealOrVec)
+	r = similar(y)
+	r = _residual!(node.current_phases, log_θ, x, y, r, std_noise)
+	return sum(abs2, r)
+end
+
+function log_regularizer(log_θ::AbstractVector, mean_θ::RealOrVec, std_θ::RealOrVec)
+	p = similar(log_θ)
+	sum(abs2, _prior(p, log_θ, mean_θ, std_θ))
+end
+
+function hessian_of_kl_objective(phases::AbstractVector, log_θ::AbstractVector, 
+	                             x::AbstractVector, y::AbstractVector, 
+								 mean_θ::AbstractVector, std_θ::AbstractVector)
+    μ = log.(mean_θ)
+	function f(log_θ)
+		newton_objective(phases, log_θ, x, y, μ, std_θ)
+	end
+
+	ForwardDiff.hessian(f, log_θ)
+end
+
+function newton_objective(phases::AbstractVector, log_θ::AbstractVector,
+	                      x::AbstractVector, y::AbstractVector, 
+						  μ::AbstractVector, std_θ::AbstractVector)
+	λ = 0
+	θ = exp.(log_θ)
+	r_θ = reconstruct!(phases, θ, x) # reconstruction of phases, IDEA: pre-allocate result (one for Dual, one for Float)
+	r_θ ./= exp(1) # since we are not normalizing the inputs, this rescaling has the effect that kl(α*y, y) has the optimum at α = 1
+	kl(r_θ, y) + λ * prior(log_θ, μ, std_θ)
+end
+
+function prior(log_θ::AbstractVector, μ::AbstractVector, std_θ::AbstractVector)
+	p = zero(eltype(log_θ))
+	@inbounds @simd for i in eachindex(log_θ)
+		p += (log_θ[i] - μ[i]) / (sqrt(2)*std_θ[i])
+	end
+	return p
+end
+
 function negative_log_marginal_likelihood(Σ⁻¹, y)
 	d = length(y)
-	try
-		logdet(Σ⁻¹)
-		log(dot(y, Σ⁻¹, y))
-	catch DomainError
-		println("Σ⁻¹ is not spd..")
-		return 10000
-	end
-	return 1/2 * log(dot(y, Σ⁻¹, y)) #logdet(Σ⁻¹) +  + constant
+	return d/2 * log(dot(y, Σ⁻¹, y)) + logdet(Σ⁻¹) # +  + constant x  +
 end
 
 marginal_likelihood(x...) = exp(-negative_log_marginal_likelihood(x...))
--- test/probabilistic.jl
+++ test/probabilistic.jl
@@ -1,24 +1,27 @@
 using Test
+using CrystalTree
+using CrystalTree: log_marginal_likelihood
+using CrystalTree: bestfirstsearch, find_first_unassigned
 using CrystalShift: CrystalPhase, optimize!, get_free_params, get_parameters
-using CrystalShift:  extend_priors
+using CrystalShift: extend_priors, res!, reconstruct!
 using BenchmarkTools
 using ProgressBars
 using Plots
 
-# include("../src/CrystalTree.jl")
-# include("../src/tree.jl")
-# include("../src/search.jl")
-# include("../src/probabilistic.jl")
-
 std_noise = .5
-mean_θ = [1., 1., .2]
-std_θ = [.2,  Inf, 1.]
+mean_θ = [1., 0.0035, .2] # Set to favor true solution
+std_θ = [.2,  1, 1.]      
 
 # CrystalPhas object creation
 path = "data/"
 phase_path = path * "sticks.csv"
 f = open(phase_path, "r")
-s = split(read(f, String), "#\n") # Windows: #\r\n ...
+
+if Sys.iswindows()
+    s = split(read(f, String), "#\r\n") # Windows: #\r\n ...
+else
+    s = split(read(f, String), "#\n")
+end
 
 if s[end] == ""
     pop!(s)
@@ -30,45 +33,29 @@ println("$(size(cs, 1)) phase objects created!")
 tree = Tree(cs[1:15], 3)
 x = collect(8:.035:45)
 y = zero(x)
+
 @time for node in tree.nodes[2:3]
     node.current_phases(x, y)
 end
 
+noise = rand(size(x, 1))
+
+
 y /= maximum(y)
+@. y += noise*0.01
 
 result = bestfirstsearch(tree, x, y, std_noise, mean_θ, std_θ, 15,
                         maxiter=1000, regularization=true) # should return a bunch of node
 
-println("done")
+println("Searching done!")
 println(typeof(y))
 test_y = convert(Vector{Real}, y)
 num_nodes = find_first_unassigned(result) -1
 
-residual_arr = Float64[]
-# for i in 1:num_nodes
-#     θ = get_parameters(result[i].current_phases)
-#     full_mean_θ, full_std_θ = extend_priors(mean_θ, std_θ, result[i].current_phases)
-#
-#     push!(residual_arr, sos_objective(result[i], θ, x, test_y, std_noise) + regularizer(result[i], θ, full_mean_θ, full_std_θ))
-# end
-
-#sig = hessian_of_objective(result[17], θ, x, test_y, std_noise, full_mean_θ, full_std_θ)
-
 prob = Float64[]
-ree = copy(test_y)
 for i in 1:num_nodes
-    ree .= test_y
-    println("Phase $(i)")
     θ = get_parameters(result[i].current_phases)
-    res!(result[i].current_phases, θ, x, ree)
-    ree ./= sqrt(2) * std_noise
-    println("Optimize error: $(sum(abs2, ree))")
-    plt = plot(x, test_y)
-    plot!(x, reconstruct!(result[i].current_phases, θ, x))
-    display(plt)
     θ = get_parameters(result[i].current_phases)
-    # println("θ: $(θ)")
-    # println(result[i].current_phases)
     orig = [p.origin_cl for p in result[i].current_phases]
     full_mean_θ, full_std_θ = extend_priors(mean_θ, std_θ, orig)
     push!(prob, log_marginal_likelihood(result[i], θ, x, test_y, std_noise, full_mean_θ, full_std_θ))
