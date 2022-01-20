71beacd70fdecc4bfbf8a4e84c99cc7e55285f01
Ming-Chiang Chang <ming@mingchiangsair.lan>
2022-01-20 16:15:18 -0500

    Update test

--- test/probabilistic.jl
+++ test/probabilistic.jl
@@ -8,7 +8,6 @@ using CrystalShift: CrystalPhase, optimize!, get_free_params
 using CrystalShift: extend_priors, res!, reconstruct!
 using ProgressBars
 using LinearAlgebra
-# using Plots
 
 
 std_noise = 1e-2
@@ -54,32 +53,17 @@ result = bestfirstsearch(tree, x, y, std_noise, mean_θ, std_θ, 15,
 
 num_nodes = find_first_unassigned(result) - 1
 
-# residual_norm = zeros(num_nodes)
-# reconstruction = zeros(length(x), num_nodes)
-# num_of_params = zeros(Int64, num_nodes)
 prob = zeros(num_nodes)
 for i in 1:num_nodes
     θ = get_free_params(result[i].current_phases)
     orig = [p.origin_cl for p in result[i].current_phases]
-    # reconstruction[:, i] = reconstruct!(result[i].current_phases, θ, x, zero(x))
     full_mean_θ, full_std_θ = extend_priors(mean_θ, std_θ, orig)
-    # num_of_params[i] = length(θ)
     prob[i] = approximate_negative_log_evidence(result[i], θ, x, y, std_noise, full_mean_θ, full_std_θ, objective)
-    # residual_norm[i] = norm(y - reconstruction[:, i])
 end
 
 i_min = argmin(prob)
 i_truth = 16
-@test i_min == i_truth # TODO: since this passes, should clean up this file and add it to testgroups
+@test i_min == i_truth
 
 end # Testprobabilitic module
-# likelihood_ratio = @. exp(-(prob[16] - prob))
 
-# plotly()
-# plt = plot(prob, yscale = :log10, label = "neg. log evidence", xlabel="Node #")
-# plot!(residual_norm, label = "residual norm")
-# plot!(num_of_params, label = "num. parameters")
-
-# display(plt)
-
-# plot!(exp.(-prob)/sum(exp.(-prob)), lable = "evidence")
