302ce43c8071f43c1ef9a8dbb7b91595125abffa
Sebastian Ament <sebastianeament@gmail.com>
2022-10-23 20:15:06 -0700

    made probability computation more numerically stable, added types for objective methods to deprecate string representation, identified race condition

--- test/probabilistic.jl
+++ test/probabilistic.jl
@@ -1,7 +1,7 @@
-module Testprobabilistic
+module TestProbabilistic
 using Test
 using CrystalTree
-using CrystalTree: approximate_negative_log_evidence
+using CrystalTree: approximate_negative_log_evidence, get_probabilities
 using CrystalTree: bestfirstsearch, find_first_unassigned, get_phase_ids, res_bfs
 using CrystalShift
 using CrystalShift: CrystalPhase, optimize!, get_free_params, Lorentz
@@ -43,29 +43,52 @@ end
 noise = rand(size(x, 1))
 
 
-y /= maximum(y)
+y /= maximum(y)  # normalize targets
 
 method = LM
-objective = "LS"
-result = res_bfs(tree, x, y, std_noise, mean_θ, std_θ, 15,
-                        method=method, objective = objective,
+objective = CrystalTree.LeastSquares()
+results = res_bfs(tree, x, y, std_noise, mean_θ, std_θ, 15,
+                        method=method, objective=objective,
                         maxiter=1000, regularization=true)
 
-num_nodes = find_first_unassigned(result) - 1
-
-prob = zeros(num_nodes)
-for i in 1:num_nodes
-    θ = get_free_params(result[i].phase_model)
+num_nodes = find_first_unassigned(results) - 1
+results = results[1:num_nodes]
+neg_log_prob = zeros(num_nodes)
+for (i, result) in enumerate(results)
+    θ = get_free_params(result.phase_model)
     # orig = [p.origin_cl for p in result[i].phase_model]
-    full_mean_θ, full_std_θ = extend_priors(mean_θ, std_θ, result[i].phase_model.CPs)
-    prob[i] = approximate_negative_log_evidence(result[i], θ, x, y, std_noise, full_mean_θ, full_std_θ, objective)
+    full_mean_θ, full_std_θ = extend_priors(mean_θ, std_θ, result.phase_model.CPs)
+    neg_log_prob[i] = approximate_negative_log_evidence(result, θ, x, y,
+                        std_noise, full_mean_θ, full_std_θ, objective)
 end
 
-i_min = argmin(prob)
+i_min = argmin(neg_log_prob)
 
-true_phase_ids = Set([0,1])
-phase_ids = Set(get_phase_ids(result[i_min]))
+true_phase_ids = Set((0, 1))
+phase_ids = Set(get_phase_ids(results[i_min]))
 @test phase_ids == true_phase_ids
 
-end # Testprobabilistic module
+# test for get_probabilities
+prob = get_probabilities(results, x, y, std_noise, mean_θ, std_θ)
 
+# validity as probability distribution
+@test sum(prob) ≈ 1
+@test all(≥(0), prob)
+
+# non-zero probabilities are in reverse sort order of negative log probabilities
+ind = @. prob != 0
+@test sortperm(prob[ind]) == reverse(sortperm(neg_log_prob[ind]))
+
+# highest-probability phase is correct
+i_min = argmax(prob)
+true_phase_ids = Set((0, 1))
+phase_ids = Set(get_phase_ids(results[i_min]))
+@test phase_ids == true_phase_ids
+
+# testing without renormalization
+prob = get_probabilities(results, x, y, std_noise, mean_θ, std_θ, renormalize=false)
+@test sum(prob) ≈ 1
+@test all(≥(0), prob)
+@test exp.(-neg_log_prob) / sum(exp.(-neg_log_prob)) ≈ prob
+
+end # TestProbabilistic module
